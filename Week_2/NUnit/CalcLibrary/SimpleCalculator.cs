d5d45805ea102f796adce44baa64314e62e208a5
Aaquib <aaquib110903@gmail.com>
2026-06-26 15:06:25 +0530

    Week 2

---
+++ Week_2/NUnit/CalcLibrary/SimpleCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CalcLibrary
+{
+    interface IMathLibrary
+    {
+        double Addition(double a, double b);
+        double Subtraction(double a, double b);
+        double Multiplication(double a, double b);
+        double Division(double a, double b);
+    }
+    public class SimpleCalculator : IMathLibrary
+    {
+        double result = 0;
+        public double Addition(double a, double b) => result = a + b;
+        public double Subtraction(double a, double b) => result = a - b;
+        public double Multiplication(double a, double b) => result = a * b;
+        public double Division(double a, double b)
+        {
+            if (b == 0)
+                throw new ArgumentException("Second Parameter Can't be Zero");
+            return result = a / b;
+        }
+        public void AllClear() => result = 0;
+        public double GetResult => result;
+    }
+}
